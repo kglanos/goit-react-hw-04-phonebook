fef1acd8deadc102be9df572f340e84eadd0bd28
kglanos <130902847+kglanos@users.noreply.github.com>
2023-11-30 23:29:05 +0100

    Update App in progress

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,18 +4,33 @@ import { ContactForm } from './contactForm/ContactForm';
 import { Filter } from './filter/Filter';
 import { ContactList } from './contactList/ContactList';
 
-export class App extends Component {
-  state = {
-    contacts: [
+const App = () => {
+  const [contacts, setContacts] = useState((
+    [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-    name: '',
-    number: '',
-  };
+    ]);
+
+    const [filter, setFilter] = useState('');
+
+    const addContactOnSubmit = (name, number) => {
+      const isContactExist = contacts.some(
+        (contact) => contact.name.toLowerCase() === name.toLowerCase(),
+      );
+
+      if (isContactExist) {
+        alert(`${name} is already in contacts`);
+        return;
+      }
+      
+      const newContact = {
+        id: nanoid(),
+        name,
+        number,
+      };
+    
 
   componentDidMount() {
     const contacts = localStorage.getItem('contacts');
